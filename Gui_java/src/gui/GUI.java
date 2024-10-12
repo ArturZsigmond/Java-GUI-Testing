5ae44e057a187ee452a5deda57ef953cd6933a9e
arturzsigmond <arthurzsigmond7@gmail.com>
2024-10-12 06:16:31 +0300

    first try, made a click counter

---
+++ Gui_java/src/gui/GUI.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+public class GUI implements ActionListener{
+
+    private int count  = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+
+        frame = new JFrame();
+
+        JButton button = new JButton("click me!");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: " + count);
+
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
+        panel.setLayout(new GridLayout());
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+    } 
+
+    public static void main(String[] args){
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+
+    }
+}
