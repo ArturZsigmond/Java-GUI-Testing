3f11cf12f94081e6f1a10264bc5840d00772965a
arturzsigmond <arthurzsigmond7@gmail.com>
2024-10-21 02:38:25 +0300

    nothing

--- Gui_java/src/gui/GUI.java
+++ Gui_java/src/gui/GUI.java
@@ -42,7 +42,7 @@ public class GUI implements ActionListener{
         frame.pack();
         frame.setVisible(true);
 
-    } 
+    }   
 
     public static void main(String[] args){
         new GUI();
